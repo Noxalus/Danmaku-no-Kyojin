042cd5687df89090febf42359c9f767183a22e33
Noxalus <noxalus@gmail.com>
2015-08-19 14:51:38 +0400

    Added HP to BossCore.

--- Danmaku no Kyojin/Danmaku no Kyojin/Entities/Boss/BossCore.cs
+++ Danmaku no Kyojin/Danmaku no Kyojin/Entities/Boss/BossCore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Danmaku_no_Kyojin.Collisions;
 using Danmaku_no_Kyojin.Sprites;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -9,11 +10,13 @@ namespace Danmaku_no_Kyojin.Entities.Boss
     {
         private readonly Entity _parent;
         private AnimatedSprite _animatedSprite;
+        private float _hp;
 
-        public BossCore(DnK gameRef, Entity parent)
+        public BossCore(DnK gameRef, Entity parent, float initialHp = 100f)
             : base(gameRef)
         {
             _parent = parent;
+            _hp = initialHp;
         }
 
         protected override void LoadContent()
@@ -21,11 +24,11 @@ namespace Danmaku_no_Kyojin.Entities.Boss
             Sprite = GameRef.Content.Load<Texture2D>("Graphics/Entities/boss_core");
 
             // TODO: Upadte AnimatedSprite class to take a "rectangle" order
-            var animation = new Animation(7, 50, 50, 0, 0, 5);
-            Origin = new Vector2(25, 25);
-            Scale = new Vector2(2f, 2f);
-            _animatedSprite = new AnimatedSprite(Sprite, animation, Position) {IsAnimating = true};
-
+            var animation = new Animation(6, 42, 38, 0, 0, 5);
+            Origin = new Vector2(21, 19);
+            Scale = new Vector2(1f, 1f);
+            _animatedSprite = new AnimatedSprite(Sprite, animation, Position) {IsAnimating = false};
+            CollisionBoxes.Add(new CollisionCircle(_parent, Vector2.Zero, 20));
             base.LoadContent();
         }
 
@@ -59,6 +62,23 @@ namespace Danmaku_no_Kyojin.Entities.Boss
             Position = position;
 
             _animatedSprite.Position = _parent.Position;
+        }
+
+        public override bool Intersects(Entity entity)
+        {
+            if (!entity.Intersects(this))
+                return false;
+
+            var bullet = entity as BaseBullet;
+            if (bullet != null)
+            {
+                _hp -= bullet.Power;
+
+                if (_hp < 0)
+                    IsAlive = false;
+            }
+
+            return true;
         }
 
         public override void Draw(GameTime gameTime)
